a797ad904fe1adf499cc0ba92028462c835ba476
Christoph Nolte <christoph.nolte@virtualminds.com>
2024-10-02 21:07:36 +0200

    Fix tooltip docs

--- src/routes/components/tooltip/+page.svelte
+++ src/routes/components/tooltip/+page.svelte
@@ -17,15 +17,19 @@
 		class="mx-auto mb-8 flex w-fit flex-wrap gap-4 rounded-3xl border border-solid border-[var(--np-divider-color)] p-4"
 	>
 		<div aria-describedby="example-tooltip-1" bind:this={exampleBtn1}>Hover over this text</div>
-		<Tooltip anchor={exampleBtn1} id="example-tooltip-1">This is a basic tooltip</Tooltip>
+		{#if exampleBtn1}
+			<Tooltip anchor={exampleBtn1} id="example-tooltip-1">This is a basic tooltip</Tooltip>
+		{/if}
 	</div>
 	<Code
 		value={`<div aria-describedby="tooltip" bind:this={buttonEl}>
 	Hover over this text
 </div>
-<Tooltip anchor={buttonEl} id="tooltip">
-	This is a basic tooltip
-</Tooltip>`}
+{#if buttonEl}
+	<Tooltip anchor={buttonEl} id="tooltip">
+		This is a basic tooltip
+	</Tooltip>
+{/if}`}
 	/>
 	<h2>Button with Tooltip</h2>
 	<p>Buttons have built-in support for tooltips. You can simply set the title attribute.</p>
