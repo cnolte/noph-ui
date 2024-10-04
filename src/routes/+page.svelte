70dc5b0fd6cf6d709046ffbb7b90f35b616f6941
Christoph Nolte <christoph.nolte@virtualminds.com>
2024-10-04 08:56:11 +0200

    Fix hero text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,8 +9,8 @@
 <div class="paper">
 	<h1>Noph UI</h1>
 	<p class="mb-12 text-2xl font-medium md:text-4xl">
-		It brings together Material Design 3, server-side rendering (SSR), lightweight performance and
-		Svelte 5.
+		It combines Material Design 3, server-side rendering (SSR), lightweight performance, and Svelte
+		5.
 	</p>
 	<Button size="large" href="/about/quick-start" style="width:fit-content">Get Started</Button>
 </div>
