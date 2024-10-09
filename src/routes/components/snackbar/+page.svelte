571d3a171e7bb91ed4e791a91d326eeb7798f948
Christoph Nolte <christoph.nolte@virtualminds.com>
2024-10-10 00:01:39 +0200

    Rename

--- src/routes/components/snackbar/+page.svelte
+++ src/routes/components/snackbar/+page.svelte
@@ -11,26 +11,26 @@
 <h1>Snackbar</h1>
 
 <h2>Single line snackbar</h2>
-<Snackbar label="Single line snackbar with action and icon" actionLabel="Dismiss"
+<Snackbar label="Single line snackbar with action and icon" actionLabel="Action"
 	>{#snippet icon()}
 		<CloseIcon />
 	{/snippet}</Snackbar
 >
 <Code
-	value={`<Snackbar label="Single line snackbar with action and icon" actionLabel="Dismiss">
+	value={`<Snackbar label="Single line snackbar with action and icon" actionLabel="Action">
 	{#snippet icon()}
 		<CloseIcon />
 	{/snippet}
 </Snackbar>`}
 />
 <h2>Two line snackbar</h2>
-<Snackbar label="Two line snackbar" supportingText="with action and icon" actionLabel="Dismiss"
+<Snackbar label="Two line snackbar" supportingText="with action and icon" actionLabel="Action"
 	>{#snippet icon()}
 		<CloseIcon />
 	{/snippet}</Snackbar
 >
 <Code
-	value={`<Snackbar label="Two line snackbar" supportingText="with action and icon" actionLabel="Dismiss">
+	value={`<Snackbar label="Two line snackbar" supportingText="with action and icon" actionLabel="Action">
 	{#snippet icon()}
 		<CloseIcon />
 	{/snippet}
