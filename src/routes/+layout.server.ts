174e874745dd7e27f8ff321f6e85baad5849dd8d
Christoph Nolte <christoph.nolte@virtualminds.com>
2024-12-02 09:01:48 +0100

    Use SSG

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,7 +1,5 @@
 import type { LayoutServerLoad } from './$types.js'
 
-export const prerender = true
-
 export const load: LayoutServerLoad = async ({ cookies }) => {
 	return {
 		theme: cookies.get('theme'),
