0f1548f9819df20bdb6387e1413881a6fae94e46
Christoph Nolte <christoph.nolte@virtualminds.com>
2024-12-15 20:27:03 +0100

    Improve css

--- src/lib/icons/Icon.svelte
+++ src/lib/icons/Icon.svelte
@@ -2,41 +2,43 @@
 	let { children } = $props()
 </script>
 
-<span class="material-symbols-outlined">{@render children()}</span>
+<svelte:head>
+	<style>
+		.material-symbols-outlined {
+			font-family: 'Material Symbols Outlined';
+			font-weight: normal;
+			font-style: normal;
+			font-size: 24px;
+			line-height: 1;
+			letter-spacing: normal;
+			text-transform: none;
+			display: inline-block;
+			white-space: nowrap;
+			word-wrap: normal;
+			direction: ltr;
+			font-feature-settings: 'liga';
+			-webkit-font-feature-settings: 'liga';
+			-webkit-font-smoothing: antialiased;
+		}
+		.material-symbols-outlined {
+			font-variation-settings:
+				'FILL' 0,
+				'wght' 400,
+				'GRAD' 0,
+				'opsz' 24;
+		}
+		:where(.button-icon) .material-symbols-outlined {
+			display: inline-flex;
+			fill: currentColor;
+			color: var(--_icon-color);
+			font-size: var(--_icon-size);
+			inline-size: var(--_icon-size);
+			block-size: var(--_icon-size);
+			flex-shrink: 0;
+			writing-mode: horizontal-tb;
+			position: relative;
+		}
+	</style>
+</svelte:head>
 
-<style>
-	:global(.material-symbols-outlined) {
-		font-family: 'Material Symbols Outlined';
-		font-weight: normal;
-		font-style: normal;
-		font-size: 24px;
-		line-height: 1;
-		letter-spacing: normal;
-		text-transform: none;
-		display: inline-block;
-		white-space: nowrap;
-		word-wrap: normal;
-		direction: ltr;
-		font-feature-settings: 'liga';
-		-webkit-font-feature-settings: 'liga';
-		-webkit-font-smoothing: antialiased;
-	}
-	.material-symbols-outlined {
-		font-variation-settings:
-			'FILL' 0,
-			'wght' 400,
-			'GRAD' 0,
-			'opsz' 24;
-	}
-	:global(:where(.button-icon) .material-symbols-outlined) {
-		display: inline-flex;
-		fill: currentColor;
-		color: var(--_icon-color);
-		font-size: var(--_icon-size);
-		inline-size: var(--_icon-size);
-		block-size: var(--_icon-size);
-		flex-shrink: 0;
-		writing-mode: horizontal-tb;
-		position: relative;
-	}
-</style>
+<span class="material-symbols-outlined">{@render children()}</span>
