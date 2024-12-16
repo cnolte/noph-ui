d08e46b32ad991f306be4b8cb8f3d9741e9de1d9
Christoph Nolte <christoph.nolte@virtualminds.com>
2024-12-16 23:33:31 +0100

    Add Radio Component

--- src/routes/components/radio/+page.svelte
+++ src/routes/components/radio/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Radio from '$lib/radio/Radio.svelte'
+	import Code from '../../Code.svelte'
 	import DemoContainer from '../../DemoContainer.svelte'
 </script>
 
@@ -9,4 +10,15 @@
 
 <h1>Radio button</h1>
 <h2>Usage</h2>
-<DemoContainer><Radio /></DemoContainer>
+<DemoContainer>
+	<form>
+		<Radio name="group" aria-label="First radio" checked />
+		<Radio name="group" aria-label="Second radio" />
+		<Radio name="group" aria-label="Third radio" />
+	</form>
+</DemoContainer>
+<Code
+	value={`<Radio name="group" aria-label="First radio" checked />
+<Radio name="group" aria-label="Second radio" />
+<Radio name="group" aria-label="Third radio" />`}
+/>
