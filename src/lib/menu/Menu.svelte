3f1649b04a0db6c546388239a826cb2ea9fc3ba1
cnolte <worldofnolte@gmail.com>
2025-09-15 13:51:28 +0200

    fix: comment out unused popover handling functions and related logic

--- src/lib/menu/Menu.svelte
+++ src/lib/menu/Menu.svelte
@@ -19,90 +19,89 @@
 	let clientHeight = $state(0)
 	let innerHeight = $state(0)
 
-	showPopover = () => {
-		element?.showPopover()
-	}
+	// showPopover = () => {
+	// 	element?.showPopover()
+	// }
 
-	hidePopover = () => {
-		element?.hidePopover()
-	}
-	const refreshValues = () => {
-		if (element && anchor && open) {
-			const anchorRect = anchor.getBoundingClientRect()
-			let maxHeight: number
-			if (innerHeight - anchorRect.bottom > anchorRect.top) {
-				maxHeight = innerHeight - anchorRect.bottom
-			} else {
-				maxHeight = anchorRect.top
-			}
-			element.style.maxHeight =
-				maxHeight > innerHeight - anchorRect.height
-					? `${innerHeight - anchorRect.height - 4}px`
-					: `${maxHeight - 4}px`
-			if (!('positionArea' in document.documentElement.style)) {
-				const docClientWidth = document.documentElement.clientWidth
-				if (anchorRect.bottom + clientHeight > innerHeight && anchorRect.top - clientHeight > 0) {
-					element.style.top = `${anchorRect.top - clientHeight - 2}px`
-				} else {
-					element.style.top = `${anchorRect.bottom + 2}px`
-				}
-				const left = anchorRect.left + anchorRect.width / 2 - clientWidth / 2
-				if (left < 2) {
-					element.style.left = '2px'
-				} else if (left > docClientWidth - clientWidth) {
-					element.style.left = `${docClientWidth - clientWidth - 4}px`
-				} else {
-					element.style.left = `${anchorRect.left + anchorRect.width / 2 - clientWidth / 2}px`
-				}
-			}
-		}
-	}
-	$effect(refreshValues)
+	// hidePopover = () => {
+	// 	element?.hidePopover()
+	// }
+	// const refreshValues = () => {
+	// 	if (element && anchor && open) {
+	// 		const anchorRect = anchor.getBoundingClientRect()
+	// 		let maxHeight: number
+	// 		if (innerHeight - anchorRect.bottom > anchorRect.top) {
+	// 			maxHeight = innerHeight - anchorRect.bottom
+	// 		} else {
+	// 			maxHeight = anchorRect.top
+	// 		}
+	// 		element.style.maxHeight =
+	// 			maxHeight > innerHeight - anchorRect.height
+	// 				? `${innerHeight - anchorRect.height - 4}px`
+	// 				: `${maxHeight - 4}px`
+	// 		if (!('positionArea' in document.documentElement.style)) {
+	// 			const docClientWidth = document.documentElement.clientWidth
+	// 			if (anchorRect.bottom + clientHeight > innerHeight && anchorRect.top - clientHeight > 0) {
+	// 				element.style.top = `${anchorRect.top - clientHeight - 2}px`
+	// 			} else {
+	// 				element.style.top = `${anchorRect.bottom + 2}px`
+	// 			}
+	// 			const left = anchorRect.left + anchorRect.width / 2 - clientWidth / 2
+	// 			if (left < 2) {
+	// 				element.style.left = '2px'
+	// 			} else if (left > docClientWidth - clientWidth) {
+	// 				element.style.left = `${docClientWidth - clientWidth - 4}px`
+	// 			} else {
+	// 				element.style.left = `${anchorRect.left + anchorRect.width / 2 - clientWidth / 2}px`
+	// 			}
+	// 		}
+	// 	}
+	// }
+	// $effect(refreshValues)
 
-	const getScrollableParent = (start: HTMLElement) => {
-		let el: HTMLElement | null = start
-		while (el) {
-			const style = getComputedStyle(el)
-			const overflowY = style.overflowY
-			const overflowX = style.overflowX
-			const isScrollableY =
-				(overflowY === 'auto' || overflowY === 'scroll') && el.scrollHeight > el.clientHeight
-			const isScrollableX =
-				(overflowX === 'auto' || overflowX === 'scroll') && el.scrollWidth > el.clientWidth
+	// const getScrollableParent = (start: HTMLElement) => {
+	// 	let el: HTMLElement | null = start
+	// 	while (el) {
+	// 		const style = getComputedStyle(el)
+	// 		const overflowY = style.overflowY
+	// 		const overflowX = style.overflowX
+	// 		const isScrollableY =
+	// 			(overflowY === 'auto' || overflowY === 'scroll') && el.scrollHeight > el.clientHeight
+	// 		const isScrollableX =
+	// 			(overflowX === 'auto' || overflowX === 'scroll') && el.scrollWidth > el.clientWidth
 
-			if (isScrollableY || isScrollableX) {
-				return el
-			}
+	// 		if (isScrollableY || isScrollableX) {
+	// 			return el
+	// 		}
 
-			el = el.parentElement
-		}
-		return window
-	}
+	// 		el = el.parentElement
+	// 	}
+	// 	return window
+	// }
 
-	const onScroll = () => {
-		refreshValues()
-	}
+	// const onScroll = () => {
+	// 	refreshValues()
+	// }
 
-	const attachScrollableParent = (el: HTMLDivElement) => {
-		if (!('anchorName' in document.documentElement.style)) {
-			getScrollableParent(el).addEventListener('scroll', onScroll, { passive: true })
-		}
-	}
+	// const attachScrollableParent = (el: HTMLDivElement) => {
+	// 	if (!('anchorName' in document.documentElement.style)) {
+	// 		getScrollableParent(el).addEventListener('scroll', onScroll, { passive: true })
+	// 	}
+	// }
 
-	onMount(() => {
-		return () => {
-			if (element && !('anchorName' in document.documentElement.style)) {
-				getScrollableParent(element).removeEventListener('scroll', onScroll)
-			}
-		}
-	})
+	// onMount(() => {
+	// 	return () => {
+	// 		if (element && !('anchorName' in document.documentElement.style)) {
+	// 			getScrollableParent(element).removeEventListener('scroll', onScroll)
+	// 		}
+	// 	}
+	// })
 </script>
 
-<svelte:window bind:innerHeight onresize={refreshValues} />
+<!-- <svelte:window bind:innerHeight onresize={refreshValues} /> -->
 <div
 	role="menu"
 	{...attributes}
-	{@attach attachScrollableParent}
 	bind:this={element}
 	bind:clientWidth
 	bind:clientHeight
@@ -145,9 +144,6 @@
 			overlay 0.2s allow-discrete,
 			opacity 0.2s linear;
 		opacity: 0;
-		justify-self: var(--np-menu-justify-self, anchor-center);
-		position-area: var(--np-menu-position-area, bottom center);
-		position-try: normal flip-block;
 	}
 
 	.np-menu-container[popover]:popover-open {
