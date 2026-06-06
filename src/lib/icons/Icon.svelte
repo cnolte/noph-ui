6a5d0ecc7b643b229b939701aa1c0746133cf258
cnolte <christoph.nolte@virtualminds.com>
2026-06-06 12:45:02 +0200

    chore: refactor Icon component styles

--- src/lib/icons/Icon.svelte
+++ src/lib/icons/Icon.svelte
@@ -9,46 +9,42 @@
 	let { children, element = $bindable(), ...attributes }: IconProps = $props()
 </script>
 
-<svelte:head>
-	<style>
-		.np-icon {
-			font-family: 'Material Symbols Outlined';
-			font-weight: normal;
-			font-style: normal;
-			font-size: var(--np-icon-size, var(--_icon-size, 1.5rem));
-			line-height: 1;
-			letter-spacing: normal;
-			text-transform: none;
-			display: inline-block;
-			white-space: nowrap;
-			word-wrap: normal;
-			direction: ltr;
-			font-feature-settings: 'liga';
-			-webkit-font-feature-settings: 'liga';
-			-webkit-font-smoothing: antialiased;
-		}
-		.np-icon {
-			transition-property: font-variation-settings;
-			transition-timing-function: ease-in;
-			transition: font-variation-settings 0.3s;
-			font-variation-settings: var(--np-icon-settings, 'FILL' 0, 'wght' 400, 'GRAD' 0, 'opsz' 24);
-		}
-		:where(.button-icon) .np-icon {
-			display: inline-flex;
-			fill: currentColor;
-			color: var(--_icon-color);
-			inline-size: var(--np-icon-size, var(--_icon-size));
-			block-size: var(--np-icon-size, var(--_icon-size));
-			flex-shrink: 0;
-			writing-mode: horizontal-tb;
-			position: relative;
-		}
-	</style>
-</svelte:head>
-
 <span
 	aria-hidden="true"
 	{...attributes}
 	class={['np-icon', 'notranslate', attributes.class]}
 	bind:this={element}>{@render children()}</span
 >
+
+<style>
+	:global(.np-icon) {
+		font-family: 'Material Symbols Outlined';
+		font-weight: normal;
+		font-style: normal;
+		font-size: var(--np-icon-size, var(--_icon-size, 1.5rem));
+		line-height: 1;
+		letter-spacing: normal;
+		text-transform: none;
+		display: inline-block;
+		white-space: nowrap;
+		word-wrap: normal;
+		direction: ltr;
+		font-feature-settings: 'liga';
+		-webkit-font-feature-settings: 'liga';
+		-webkit-font-smoothing: antialiased;
+		transition-property: font-variation-settings;
+		transition-timing-function: ease-in;
+		transition: font-variation-settings 0.3s;
+		font-variation-settings: var(--np-icon-settings, 'FILL' 0, 'wght' 400, 'GRAD' 0, 'opsz' 24);
+	}
+	:global(:where(.button-icon) .np-icon) {
+		display: inline-flex;
+		fill: currentColor;
+		color: var(--_icon-color);
+		inline-size: var(--np-icon-size, var(--_icon-size));
+		block-size: var(--np-icon-size, var(--_icon-size));
+		flex-shrink: 0;
+		writing-mode: horizontal-tb;
+		position: relative;
+	}
+</style>
